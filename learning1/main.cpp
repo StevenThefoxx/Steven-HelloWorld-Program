02d0a5d162c8e51c83fc9c569faacd16f0745afb
StevenTheFox <39722169+StevenThefoxx@users.noreply.github.com>
2025-06-01 18:38:30 -0400

    first upload
    could be buggy

---
+++ learning1/main.cpp
@@ -0,0 +1,15 @@
+#include  <iostream> // include is a prestart processor directive, iostream used for input/output
+
+
+using namespace std; //c++ uses namespaces to organize names to code blocks, this says to use the std namespace
+
+int main() // c++ is broken up into functions, main is the entry point of the program, one statement at a time
+
+{
+
+// iostream is a library that allows us to use see out object "cout" which in turns help us output "hello world" text
+
+    cout << "hello, world\n"; // << is the output operator, \n is the new line operator
+
+    return 0; // return value of 0 means the program ran successfully
+}
